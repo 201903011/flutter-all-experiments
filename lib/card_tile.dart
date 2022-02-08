a776231633a51b8137f3f366ac8e249dd5a6d8ae
201903011 <72931833+201903011@users.noreply.github.com>
2022-02-08 16:24:10 +0530

    experiment5

---
+++ lib/card_tile.dart
@@ -0,0 +1,25 @@
+import 'package:experiment4/Prod_Image.dart';
+import 'package:flutter/material.dart';
+
+import 'dart:convert';
+
+class Prod1 extends StatelessWidget {
+  final double w1;
+  final String im;
+  const Prod1({Key? key, required this.w1, required this.im}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Prod_Img(
+        image: im,
+        h: 150,
+        w: w1 / 2 - 20,
+      ),
+    );
+  }
+}
